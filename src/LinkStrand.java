ac7eaf44134925797ffb0967932bd83aab337f7d
Tanisha Nalavadi <tn85@duke.edu>
2018-10-25 22:19:45 -0400

    version13

--- src/LinkStrand.java
+++ src/LinkStrand.java
@@ -1,32 +1,32 @@
 import java.lang.IndexOutOfBoundsException;
 public class LinkStrand implements IDnaStrand{
 	private LinkStrand myInfo;
-	
-	private class Node {
-	   	String info;
-	   	Node next;
-	   	public Node(String s) {
-	      	info = s;
-	      	next = null;
-	   	}
-	   }
-	   private Node myFirst,myLast;
-	   private long mySize;
-	   private int myAppends;
-	   private int myIndex;
-	   private int myLocalIndex;
-	   private Node myCurrent;
 
-	
+	private class Node {
+		String info;
+		Node next;
+		public Node(String s) {
+			info = s;
+			next = null;
+		}
+	}
+	private Node myFirst,myLast;
+	private long mySize;
+	private int myAppends;
+	private int myIndex;
+	private int myLocalIndex;
+	private Node myCurrent;
+
+
 	public LinkStrand(String s) {
 		initialize(s);
 	}
-	
+
 	public LinkStrand() {
 		this("");
 	}
-	
-	
+
+
 
 	@Override
 	public long size() {
@@ -57,58 +57,51 @@ public class LinkStrand implements IDnaStrand{
 			myLast= new Node(dna);
 			myFirst.next=myLast;
 		}else {
-		Node pointer= new Node(dna);
-		myLast.next=pointer;
-		myLast=myLast.next;}
-	
+			Node pointer= new Node(dna);
+			myLast.next=pointer;
+			myLast=myLast.next;}
+
 		return this;
 	}
 
 	@Override
 	public IDnaStrand reverse() {
-		LinkStrand ans= new LinkStrand();
-		Node pointer= myFirst;
-		if (this==null) {
-			return null;}
-		
 		if (myFirst.next==null) {
 			StringBuilder thang= new StringBuilder(myFirst.info);
 			thang=thang.reverse();
 			LinkStrand answer=new LinkStrand(thang.toString());
 			return answer;
 		}
-	
-		else {
-			Node p1=myFirst;
-			StringBuilder thing2= new StringBuilder (p1.info);
-			thing2=thing2.reverse();		
-			LinkStrand copy= new LinkStrand(thing2.toString());
-			Node head=copy.myFirst;
+		Node p1=myFirst;
+		StringBuilder thing2= new StringBuilder (p1.info);
+		thing2.reverse();		
+		LinkStrand copy= new LinkStrand(thing2.toString());
+		Node head=copy.myFirst;
+		p1=p1.next;
+		while (p1!=null) {
+			StringBuilder thing= new StringBuilder (p1.info);
+			thing.reverse();	
+			copy.append(thing.toString());
 			p1=p1.next;
-			while (p1!=null) {
-				StringBuilder thing= new StringBuilder (p1.info);
-				thing=thing.reverse();	
-				copy.append(thing.toString());
-				p1=p1.next;
-			}		
-			Node current=head;
-			Node prev=null;
-			Node next=head.next;
-			while (current!=null) {
-				current.next=prev;
-				prev=current;
-				current=next;
-				if (current!=null) {
-					next=current.next;	
-				}
+		}		
+		Node current=head;
+		Node prev=null;
+		Node next=head.next;
+		while (current!=null) {
+			current.next=prev;
+			prev=current;
+			current=next;
+			if (current!=null) {
+				next=current.next;	
 			}
-			copy.myFirst=myLast;
-			copy.myLast=p1;
-			return copy;
-			}
-			
-		} 
-		
+		}
+		copy.myFirst=copy.myLast;
+
+		return copy;
+
+
+	} 
+
 
 	@Override
 	public int getAppendCount() {
@@ -136,9 +129,9 @@ public class LinkStrand implements IDnaStrand{
 		myLocalIndex += differnce; 
 		return myCurrent.info.charAt(myLocalIndex);
 	}
-	
 
-	
+
+
 	@Override
 	public String toString(){
 		StringBuilder ans = new StringBuilder();
